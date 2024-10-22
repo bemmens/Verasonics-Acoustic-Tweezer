75159a639cb9d450ccc2adcf9ac12b22f723c8b7
bemmens <104365870+bemmens@users.noreply.github.com>
2024-10-22 13:45:37 +0100

    misc

--- SetUp Files/SetUp_SmoothMove_BugFixing.m
+++ SetUp Files/SetUp_SmoothMove_BugFixing.m
@@ -22,8 +22,8 @@ bigDishDIAm = 90e3/wavelength;
 TPC(5).maxHighVoltage = 20;
 
 %% Generate TW
-pulseLength = 20; % ms
-nHalfCycles = int32(2*pulseLength*Trans.frequency);
+%pulseLength = 20; % ms
+%nHalfCycles = int32(2*pulseLength*Trans.frequency);
 TW(1).type = 'parametric'; 
 TW(1).Parameters = [Trans.frequency,0.9,100,1]; % A, B, C, D
 TW(1).equalize = 0;
