8af10e92321177e7366562529c3e042735a2418c
bemmens <104365870+bemmens@users.noreply.github.com>
2025-10-03 16:11:02 +0100

    Tx Testing Script

---
+++ TransducerData/Channel Testing/TiePie_ChannelTesting.m
@@ -0,0 +1,146 @@
+%% Details
+% Description
+
+clear all;
+close all;
+clc;
+fclose all;
+
+%% Connect to HandyScope
+disp('Connecting to HandyScope...')
+% Open LibTiePie and display library info if not yet opened:
+import LibTiePie.Const.*
+import LibTiePie.Enum.*
+
+if ~exist('LibTiePie', 'var')
+  % Open LibTiePie:
+  LibTiePie = LibTiePie.Library;
+else
+    disp('Library Connection Failed');
+end
+
+% Search for devices:
+LibTiePie.DeviceList.update()
+
+% Try to open an oscilloscope with block measurement support:
+clear scp;
+for k = 0 : LibTiePie.DeviceList.Count - 1
+    item = LibTiePie.DeviceList.getItemByIndex(k);
+    if item.canOpen(DEVICETYPE.OSCILLOSCOPE)
+        scp = item.openOscilloscope();
+        if ismember(MM.BLOCK, scp.MeasureModes)
+            break;
+        else
+            clear scp;
+            disp('Cleared Scope')
+        end
+    end
+end
+clear item
+disp('Connected')
+%% SET OSCILLOSCOPE SETTINGS
+
+if exist('scp', 'var')
+    % Set measure mode:
+    scp.MeasureMode = 2; % Block Mode
+
+    % Set sample frequency:
+    MHz = 50;     % CHECK
+    scp.SampleFrequency = MHz*1e6; %  MHz
+
+    % Set record length:
+    record_time = 100e-6; % s                % CHECK
+    scp.RecordLength = scp.SampleFrequency*record_time; % n Samples: max = 33553920 ~ 3e7 (67107840?)    
+
+    % Set pre sample ratio:
+    scp.PreSampleRatio = 0; 
+
+    % For all channels:
+    for ch = scp.Channels
+        % Enable channel to measure it:
+        ch.Enabled = true;
+        
+        % Set coupling:
+        ch.Coupling = 1; % DC Volt
+
+        % Release reference:
+        clear ch;
+    end
+    
+    % Trigger settings
+    % Set trigger timeout: 
+    scp.TriggerTimeOut = 5; % s -> Long delay to indicate trigger not found
+    
+    % Disable all channel trigger sources:
+    for ch = scp.Channels
+        ch.Trigger.Enabled = false;
+        clear ch;
+    end
+    
+    % Setup channel trigger:
+    chN = 2; % trigger channel
+    chTr = scp.Channels(chN).Trigger; 
+    chTr.Enabled = true;
+    chTr.Kind = TK.FALLINGEDGE;
+    chTr.Levels(1) = 0.5; % Trigger Level (%: 0.5=>50%)
+    chTr.Hystereses(1) = 0.1; % Hystereses (%)
+    
+    % Release reference:
+    clear chTr;
+    
+    % Set range on each channel (V)
+    scp.Channels(1).Range = 0.5 ;     % CHECK
+    scp.Channels(2).Range = 5 ;     % CHECK
+    
+else
+    warning('No Scope Detected')
+end
+
+scpSettings.RecordLength = scp.RecordLength;
+scpSettings.SampleFrequency = scp.SampleFrequency;
+scpSettings.scp.PreSampleRatio = scp.PreSampleRatio; 
+
+disp(strcat('Record time per measurement:',string(record_time*1e6),'us.'))
+
+disp('scp.SampleFrequency & redord time [MHz,us]:')
+disp(scp.SampleFrequency/1e6)
+disp(record_time*1e6)
+
+%%
+saveData.data = zeros(scpSettings.RecordLength); % counter,wvfm
+
+t = (1:scpSettings.RecordLength)*1e6/scpSettings.SampleFrequency; % us
+
+scpSettings.timestamp = datetime; % start time of day
+
+% Take measurement
+%disp('Measuring...')
+[scp, measurement] = takeMeasOscilloscope( scp );
+wvfm = measurement(:,1);
+trigger = measurement(:,2);
+
+figure(1)
+plot(t,wvfm)
+xlabel('Time [us]');
+ylabel('Voltage [V]');
+
+pause(refreshRate)
+counter = counter + 1;
+if counter > maxRunTime/refreshRate
+    warning('saveData overflowwing!')
+end
+
+%%
+File_loc = 'Verasonics-Acoustic-Tweezer/TransducerData/Channel Testing/'; % CHECK
+File_name = 'Tx_n_Probe_m'; % CHECK
+Save_String=strcat(File_loc,File_name,'.mat');
+
+% Ask for input to confirm save
+confirmSave = input('Do you want to save the data? (y/n): ', 's');
+if lower(confirmSave) ~= 'y'
+    disp('Data not saved.');
+    return;
+end
+
+save(Save_String,'saveData','scpSettings',"-v7.3");
+disp(strcat('File Saved: Data\',File_name,'.mat'));
